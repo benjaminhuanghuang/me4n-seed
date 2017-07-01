40b3c51e018b35acad981b3ff136b19a4b70dc4b
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-06-30 18:10:57 -0700

    list component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
+import { MessageListComponent } from "./messages/message-list.component";
 import {MessageComponent} from "./messages/message.component";
-
+ 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
+        MessageListComponent,
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
