799d9d68d7a600fcd1250ccb0eda35549c3c8ef7
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-03 11:31:53 -0700

    delete message

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 import { Message } from "./message.model";
+import { MessageService } from "./message.service";
 
 @Component({
     selector: 'app-message',
@@ -10,12 +11,17 @@ export class MessageComponent {
     @Input() message: Message;   // value can be passed from parent component
     @Output() editClicked = new EventEmitter<string>();
 
+    // inject same instance with message-input component
+    constructor(private messageService: MessageService) {
+
+    }
+
     color = "red";
     onEdit() {
         this.editClicked.emit('A new value');   // refer as $event
     }
 
     onDelete() {
-
+        this.messageService.deleteMessage(this.message);
     }
 }
